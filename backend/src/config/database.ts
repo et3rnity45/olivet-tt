74ce36267b15b0d855386b9da72a936f9719fe5e
Quentin Liger <qliger.dev@gmail.com>
2021-10-07 15:32:43 +0200

    update mongoose typegoose

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,20 +1,12 @@
 /* eslint-disable no-console */
-import { connect } from 'mongoose';
+import mongoose from "mongoose";
 
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/usmolivet';
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  autoIndex: true,
-};
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/usmolivet";
 
 export default async function connectDB(): Promise<void> {
   try {
-    await connect(dbUrl, options);
-    console.log('✔️  Connected to database');
+    await mongoose.connect(dbUrl, { autoIndex: true });
+    console.log("✔️  Connected to database");
   } catch (err) {
     console.log(`❌ Error during database connection : ${err}`);
   }
