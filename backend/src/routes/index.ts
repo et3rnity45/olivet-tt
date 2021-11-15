24c335e9eb906d19584e6aa612b4d67351810f2b
Quentin Liger <qliger.dev@gmail.com>
2021-11-15 19:47:04 +0100

    fix callback

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 router.post('/helloasso', (req, res) => {
-  console.log(req.body);
   res.send({
-    success: 'Bien reçu !',
+    success: req.body,
   });
 });
 
